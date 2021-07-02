e1ad7d4f1ddd70f6c383b90d31204c92a5d91212
akkaya-ardaa <ardaakkaqya533@gmail.com>
2021-07-02 16:34:53 +0300

    2 Temmuz 2021 - 16:34

--- WebAPI/Controllers/FilesController.cs
+++ WebAPI/Controllers/FilesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +12,34 @@ namespace WebAPI.Controllers
     [ApiController]
     public class FilesController : ControllerBase
     {
+        [HttpGet("listarticleimages")]
+        public async Task<IActionResult> ListArticleImages()
+        {
+            var result = Directory.GetFiles("images/article");
+            var temp = new List<string>() { };
+            foreach (var item in result)
+            {
+                temp.Add(item.Split("\\")[1]);
+            }
+            return await Task.FromResult(Ok(temp));
+        }
+
+        [HttpGet("listgalleryimages")]
+        public async Task<IActionResult> ListGalleryImages()
+        {
+            var result = Directory.GetFiles("images/gallery");
+            var temp = new List<string>() { };
+            foreach (var item in result)
+            {
+                temp.Add(item.Split("\\")[1]);
+            }
+            return await Task.FromResult(Ok(temp));
+        }
+
         [HttpGet("showarticleimage")]
         public async Task<IActionResult> ShowArticleImage([FromQuery] int id)
         {
-            Byte[] imageBytes = await System.IO.File.ReadAllBytesAsync($"images/articles/{id}");
+            Byte[] imageBytes = await System.IO.File.ReadAllBytesAsync($"images/article/{id}");
             return await Task.FromResult(File(imageBytes,"image/png"));
         }
 
@@ -23,6 +48,26 @@ namespace WebAPI.Controllers
         {
             Byte[] imageBytes = await System.IO.File.ReadAllBytesAsync($"images/gallery/{id}");
             return await Task.FromResult(File(imageBytes, "image/png"));
+        }
+
+        [HttpPost("uploadarticleimage")]
+        public async Task<IActionResult> UploadArticleImage([FromForm] IFormFile formFile)
+        {
+            var id = new Random().Next(111111111, 999999999);
+            Stream stream = new FileStream($"images/article/{id}", FileMode.CreateNew);
+            await formFile.CopyToAsync(stream);
+            stream.Close();
+            return await Task.FromResult(Ok(id));
+        }
+
+        [HttpPost("uploadgalleryimage")]
+        public async Task<IActionResult> UploadGalleryImage([FromForm] IFormFile formFile)
+        {
+            var id = new Random().Next(111111111, 999999999);
+            Stream stream = new FileStream($"images/gallery/{id}", FileMode.CreateNew);
+            await formFile.CopyToAsync(stream);
+            stream.Close();
+            return await Task.FromResult(Ok(id));
         }
     }
 }
